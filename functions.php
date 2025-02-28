782d05b8b8c6723041d487b7a629f6d3668ba2db
Hadia Shahid <hadiashahid202@gmail.com>
2025-02-28 14:44:49 +0500

    Initial commit

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+function mytheme_setup() {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    register_nav_menus(array(
+        'primary' => __('Primary Menu', 'mytheme'),
+    ));
+}
+add_action('after_setup_theme', 'mytheme_setup');
